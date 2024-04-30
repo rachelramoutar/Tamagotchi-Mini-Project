ab41af5245b5ed39d8077de95cb5bfff903d9cc0
Rachel Ramoutar <rachelramoutar1@gmail.com>
2024-04-30 19:03:06 -0400

    A good chunk done

--- script.js
+++ script.js
@@ -1,4 +1,177 @@
 // alert("Happy TamaGotcha Day! You just adopted your first TamaGotchi. Now, let's keep it alive!")
+const tamaGotchaName = document.querySelector('#thename');
 
-button feedMe = document.querySelector
-    
+// let egg = prompt("Congratulations! You have a beautiful baby egg ^.^ Let's give the baby a name", "");
+
+/* need to remove background from Gif egg*/
+const nightTime = "linear-gradient(to bottom, #0c0fa8, #37073a, #2e2c2d)";
+const dayTime = "linear-gradient(to bottom, #480ca8, #b5179e, #FF6BDB)";
+
+
+const sleepButton = document.querySelector("#sleeps")
+const awakeButton1 = document.querySelector("#awake1");
+const awakeButton2 = document.querySelector("#awake2");
+
+const audio1 = document.querySelector('#audioPlayer1');
+const audio2 = document.querySelector('#audioPlayer2');
+
+let hungerNum = document.querySelector('#hungerNum');
+let sleepinessNum = document.querySelector('#sleepinessNum');
+let boredomNum = document.querySelector('#boredomNum'); 
+let ageNum = document.querySelector("#ageNum");
+
+let fox1 = document.querySelector('#fox1');
+let fox2 = document.querySelector('#fox2');
+let fox3 = document.querySelector('#fox3');
+
+let hunger = 0; 
+let sleepiness = 0;
+let boredom = 0; 
+let age = 0;
+
+
+// if(egg ==''){
+//     prompt("TamaGotchis need love. If you won't give them a name, please try another game.", "");
+//     tamaGotchaName.innerHTML = egg;
+// }
+
+// tamaGotchaName.innerHTML = egg;
+
+const buttons = [{
+    name: "hunger",
+    "button function": ["feedPet"]
+}, { 
+    name: "sleepiness", 
+    "button function": ["sleepPet"]
+}, {
+    name: "boredom",
+    "button function": ["playPet"]
+}
+]
+
+function feedPet(){
+    hunger--;
+    age += 1;
+    hungerNum.innerText = hunger;
+    ageNum.innerText = age; 
+};
+
+function sleepPet(){ 
+    sleepiness--; 
+    age +=1; 
+    sleepinessNum.innerText = sleepiness; 
+    ageNum.innerText = age; 
+};
+
+function playPet(){
+    boredom--; 
+    age += 0.5; 
+    boredomNum.innerText = boredom; 
+    ageNum.innerText = age; 
+}
+
+
+function petDeath (){ 
+    if (age === 10){
+        alert("Grief is the cost of love. Thank you for loving your tamagotchi for such a long time. It's in a better place now."
+        ); 
+    } else if (hunger === 10){ 
+        alert("Your tamagotchi died of hunger X.X"); 
+    } else if (boredom === 10){
+        alert("Time Management would've been good here. Your tamagotchi died of boredom");
+    } else if (sleepiness === 10){
+        alert("Why are you up so late all the time? Your tamagotchi died from lack of sleep.")
+    }
+};
+
+
+egg.hidden=false;
+
+
+let timer1 = setInterval(function(){
+    morph();
+    age++;
+}, 10000); 
+
+function morph (){  
+    console.log(age)
+    if (age>=5){ 
+        egg.hidden=true;
+        fox1.hidden=false; 
+        console.log("fox1");
+    } else{
+        egg.hidden=false; 
+    };
+};
+
+let timer2 = setInterval(function(){
+    morph2();
+    age++
+}, 20000);
+
+    function morph2 (){
+        console.log(age)
+       if (age>=10){
+        fox2.hidden=true; 
+        fox3.hidden=false; 
+ } else {
+        fox2.hidden = false; 
+ }
+};
+
+let timer3 = setInterval(function(){
+    morph3();
+}, 30000);
+    function morph3 (){
+        console.log(age)
+       if (age>=15){
+        fox2.hidden=true; 
+        fox3.hidden=false; 
+ } else {
+        fox2.hidden = false; 
+ }
+};
+
+
+
+// audio1.play()
+
+document.body.style.background = dayTime; 
+function changeBackgroundColorForSleep() {
+    document.body.style.background = nightTime; 
+    // audio1.pause()
+    // audio2.play()
+}
+
+function changeBackgroundColorForAwake() {
+    document.body.style.background = dayTime; 
+    // audio1.play()
+    // audio2.pause()
+}
+sleepButton.addEventListener("click", changeBackgroundColorForSleep);
+awakeButton1.addEventListener("click", changeBackgroundColorForAwake);
+awakeButton2.addEventListener("click", changeBackgroundColorForAwake);
+
+
+
+// class Tamagotchi{
+//     constructor (name) {
+//         this.name = name; 
+//         this.hunger = 0;
+//         this.sleepiness = 0; 
+//         this.boredom = 0; 
+//         this.age = 0; 
+        
+//     }
+// }
+
+
+// audio2.hidden=true;
+// audio1.hidden=true;
+// audio1.play();
+
+
+// audio2.autoplay=false;
+
+
+
